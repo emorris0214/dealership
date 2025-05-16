2f281309f81fa9785688d0aaea6e03cd4f89a86a
unknown <Enoch Morris>
2025-05-16 13:03:04 -0400

    Completed

---
+++ src/main/java/com/pluralsight/Contract.java
@@ -0,0 +1,9 @@
+package com.pluralsight;
+
+import java.time.LocalDate;
+public abstract class Contract {
+    protected String date;
+    protected String customerName;
+    protected String customerEmail;
+    protected Vehicle
+}
